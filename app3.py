5babd812364399f6c6a626adf8d8f489a2a072e9
Hector Nunez <jesushex2@gmail.com>
2025-04-05 15:48:20 -0700

    Modifications1

--- app3.py
+++ app3.py
@@ -2,22 +2,25 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+st.title(":red[MY FIST APP]")
+
 # ORIGINAL TABLE
+st.header("ORIGINAL TABLE: ", divider="red")
+
 dataFrame = pd.read_csv("internet_usage.csv")
 st.write(dataFrame)
 
 # Display top 5 countries by internet usage in 2023 (table and bar chart)#
+st.subheader("Top 5 countries by internet usage in 2023", divider="green")
 
 # filter for column 2023
 info23 = dataFrame.loc[:, ["Country Name", "Country Code", "2023"]]
 info23 = info23[info23["2023"] > "0"]  # discard columns without a numeric sequence
 st.write(info23)
 
-
 # filter for 5 max values from the df
 info23["2023"] = pd.to_numeric(info23["2023"])
 MaxValues_23 = info23.nlargest(5, "2023").sort_values("2023", ascending=True)
-print(MaxValues_23)
 
 # table (Top 5)
 st.write(MaxValues_23)
@@ -27,6 +30,7 @@ st.line_chart(MaxValues_23, x="Country Name", y="2023")
 
 
 # Visualize internet usage in North America Counties (line chart) - from 2013 to 2023
+st.subheader("Internet usage in NA countries (2013-2023): ", divider="blue")
 
 # filter north america countries
 info_NthAm = dataFrame[
@@ -94,17 +98,16 @@ info_NthAm = info_NthAm.loc[
 # line chart
 st.line_chart(info_NthAm, x="Country Name", y=targetYear)
 
-
 # Add a filter (input widget: multiselect) with values "USA, Mexico, Canada" for filtering line chart
-targetCount = ["United States", "Mexico", "Canada"]
+st.subheader("Filter for USA, MEXICO and CANADA: ", divider="blue")
 
+targetCount = ["United States", "Mexico", "Canada"]
 info_targetCount = info_NthAm[info_NthAm["Country Name"].isin(targetCount)]
 
 # Table (USA, MEXICO, CANADA)
 st.write(info_targetCount)
-
-
 options = st.multiselect("OPTIONS: ", targetCount, default=targetCount)
+
 # chart (USA, MEXICO, CANADA)
 st.line_chart(
     info_targetCount[info_targetCount["Country Name"].isin(options)],
@@ -114,16 +117,16 @@ st.line_chart(
 
 
 # Calculate growth rate for period 2021-2022 (add new column to current DataFrame using pandas,  column name example: "growth_rate_2023")
+st.subheader("Growth 2021-2022", divider="violet")
+
 dataFrame["2021"] = dataFrame["2021"].replace("..", 0).astype(float)
 dataFrame["2022"] = dataFrame["2022"].replace("..", 0).astype(float)
-
 dataFrame["growth"] = (
     (dataFrame["2022"] - dataFrame["2021"]) / dataFrame["2021"]
 ) * 100
-# st.write(dataFrame.loc[:, ["Country Name", "2021", "2022", "growth"]])
-
+st.write(dataFrame.loc[:, ["Country Name", "2021", "2022", "growth"]])
 
 # Display top 5 countries by "growth_rate_2023" (bar chart)
-
+st.subheader("Top 5: ", divider="violet")
 dataFrame = dataFrame.nlargest(5, "growth")
 st.write(dataFrame.loc[:, ["Country Name", "2021", "2022", "growth"]])
